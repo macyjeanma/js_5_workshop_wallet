cfa29e6034b52d2dc9601e0f95261941e6f5c986
jean <macyjeanma@gmail.com>
2020-08-19 14:53:10 +0800

    update a new branch

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 window.addEventListener("DOMContentLoaded", function () {
   document.querySelector(".btn-primary").addEventListener("click", function (e) {
     e.preventDefault()
-
     const addForms = document.forms[1]
-    console.log(JSON.stringify(
+    let a = JSON.stringify(
       {
         date: addForms[1].value,
         category: addForms[0].value,
         description: addForms[3].value,
         amount: addForms[2].value
       }
-    ))
+    )
+    console.log(a)
     addForms.reset()
+    localStorage.setItem('records', a);
+
 
 
 
